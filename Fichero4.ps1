68d654112f75ca70557b98bc66927129f297d021
Aten3D <atenlladojr@hotmail.com>
2024-11-20 20:22:47 +0100

    Tarea 5.1

--- Fichero4.ps1
+++ Fichero4.ps1
@@ -5,22 +5,51 @@ function showProcesos{
 
 #2. Pida por pantalla el nombre de un proceso y  Detenerlo. Realiza una espera hasta que se haya detenido por completo el proceso anterior.
 function stopProceso{
-
+    $nombre= Read-Host "Nombre del proceso para denerlo"
+    $proceso = Get-Process -Name $nombre -ErrorAction Stop
+    Stop-Process -InputObject $proceso -Force
+    Write-Output "Esperando a que el proceso $nombre se detenga..."
+        for ($i = 0; $i -lt $segundos; $i++) {
+            if (-not (Get-Process -Name $nombreProceso -ErrorAction SilentlyContinue)) {
+                Write-Output "El proceso $nombreProceso se detuvo antes del límite de tiempo."
+                return
+            }
+            Start-Sleep -Seconds 1
+        }
+    Write-Output "El proceso $nombre se ha detenido por completo."
 }
 
 #3. Pedir el nombre de un procesos y espera durante unos segundos (pedidos por pantalla) que se detenga el proceso indicado.
 function stopProcesoDelay{
-
+    $nombre= Read-Host "Nombre del proceso para denerlo"
+    $segundos= Read-Host "Cuantos segundos espero a que el proceso se apague"
+    $proceso = Get-Process -Name $nombre -ErrorAction Stop
+    Stop-Process -InputObject $proceso -Force
+    Write-Output "Esperando a que el proceso $nombre se detenga..."
+        for ($i = 0; $i -lt $segundos; $i++) {
+            if (-not (Get-Process -Name $nombre -ErrorAction SilentlyContinue)) {
+                Write-Output "El proceso $nombre se detuvo antes del límite de tiempo."
+                return
+            }
+            Start-Sleep -Seconds 1
+        }
+    Write-Output "El proceso $nombre se ha detenido por completo."
 }
 
 #4. Inicia el Notepad.exe proceso. Maximiza la ventana y la conserva hasta que se finalice el proceso.
 function startNote{
-
+    $notepad = Start-Process -FilePath "notepad.exe" -PassThru
+    Write-Output "Notepad iniciado con PID $($notepad.Id)."
+    while ($notepad.HasExited -eq $false) {
+        Start-Sleep -Seconds 1
+    }
+    Write-Output "Notepad se ha cerrado."
 }
 
 #5. Inicia PowerShell mediante la opción Ejecutar como administrador
 function startPSAdmin{
-
+    Start-Process -FilePath "powershell.exe" -Verb RunAs
+    Write-Output "PowerShell iniciado como administrador."
 }
 
 
